7d762a055c83230b97112bd26137b039832d4e20
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-01-31 15:24:44 +0700

    Update ICustomDesign.cs

--- INBS/INBS.Application/IServices/ICustomDesign.cs
+++ INBS/INBS.Application/IServices/ICustomDesign.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using INBS.Application.DTOs.Booking.CustomNailDesign;
+using INBS.Application.DTOs.Design.CustomDesign;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,12 @@ namespace INBS.Application.IServices
 {
     public interface ICustomDesign
     {
+        Task<IEnumerable<CustomDesignResponse>> Get();
+
+        Task Create(ClaimsPrincipal claims, CustomDesignRequest request, IList<CustomNailDesignRequest> customNailDesignRequests);
+
+        Task Delete(Guid id);
+
+        Task Update(ClaimsPrincipal claims, Guid id, CustomDesignRequest request, IList<CustomNailDesignRequest> customNailDesignRequests);
     }
 }
