abed998ead32d7518f9867d3ad6b525702ef7290
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-02-20 10:20:24 +0700

    Create WebhookController.cs

---
+++ INBS/INBS.API/Controllers/WebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INBS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebhookController() : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                return Ok("DONE");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
