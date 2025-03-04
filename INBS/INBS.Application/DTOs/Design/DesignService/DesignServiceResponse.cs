179992f31bb456db18a9905963921d051631b9cf
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-04 23:01:53 +0700

    fix bug

--- INBS/INBS.Application/DTOs/Design/DesignService/DesignServiceResponse.cs
+++ INBS/INBS.Application/DTOs/Design/DesignService/DesignServiceResponse.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using INBS.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace INBS.Application.DTOs.Design.DesignService
 {
     public class DesignServiceResponse
     {
+        [Key]
+        public Guid DesignId { get; set; }
+        public virtual Domain.Entities.Design? Design { get; set; }
+
+        [Key]
+        public Guid ServiceId { get; set; }
+        public virtual Domain.Entities.Service? Service { get; set; }
     }
 }
