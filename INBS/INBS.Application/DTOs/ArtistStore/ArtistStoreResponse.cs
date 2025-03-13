1a5e641b266a2116dc266127f5dc1359eb13bc98
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:25:57 +0700

    fix

---
+++ INBS/INBS.Application/DTOs/ArtistStore/ArtistStoreResponse.cs
@@ -0,0 +1,33 @@
+﻿using INBS.Application.DTOs.Artist;
+using INBS.Application.DTOs.Store;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.ArtistStore
+{
+    public class ArtistStoreResponse
+    {
+        [Key]
+        public Guid ArtistId { get; set; }
+
+        public virtual ArtistResponse? Artist { get; set; }
+
+        [Key]
+        public Guid StoreId { get; set; }
+
+        public virtual StoreResponse? Store { get; set; }
+
+        public DateOnly WorkingDate { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
+#warning response booking
+    }
+}
--- INBS/INBS.Application/DTOs/Booking/BookingResponse.cs
+++ INBS/INBS.Application/DTOs/Booking/BookingResponse.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using INBS.Application.DTOs.ArtistStore;
+using INBS.Application.DTOs.Common;
+using INBS.Application.DTOs.CustomerSelected;
+using INBS.Application.DTOs.Feedback;
+using INBS.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace INBS.Application.DTOs.Booking
 {
-    public class BookingResponse
+    public class BookingResponse : BaseEntity
     {
+        public BookingResponse() : base() { }
+
+        public DateOnly ServiceDate { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly PredictEndTime { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+        public long TotalAmount { get; set; }
+
+        public Guid CustomerSelectedId { get; set; }
+
+        public virtual CustomerSelectedResponse? CustomerSelected { get; set; }
+
+        public Guid ArtistStoreId { get; set; }
+        
+        public virtual ArtistStoreResponse? ArtistStore { get; set; }
+
+        //public virtual ICollection<Cancellation> Cancellations { get; set; } = [];
+
+        public virtual ICollection<FeedbackResponse> Feedbacks { get; set; } = [];
     }
 }
