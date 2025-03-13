acb78e20eda402a7e0e0123ceedf8b28ab2b85fa
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:26:47 +0700

    fix

---
+++ INBS/INBS.Application/DTOs/Design/DesignRequest.cs
@@ -0,0 +1,17 @@
+﻿using INBS.Application.DTOs.NailDesign;
+
+namespace INBS.Application.DTOs.Design
+{
+    public class DesignRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public float TrendScore { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Price { get; set; }
+
+        public IList<NailDesignServiceRequest> Services { get; set; } = [];
+    }
+}
