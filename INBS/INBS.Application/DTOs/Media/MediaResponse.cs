4f792036d5a4bc44eb249eb9c5e10a27ec749c1d
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:27:26 +0700

    change business

---
+++ INBS/INBS.Application/DTOs/Media/MediaResponse.cs
@@ -0,0 +1,22 @@
+﻿using INBS.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.Image
+{
+    public class MediaResponse
+    {
+        [Key]
+        public int NumerialOrder { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Key]
+        public Guid DesignId { get; set; }
+        public int MediaType { get; set; }
+    }
+}
---
+++ INBS/INBS.Application/DTOs/NailDesign/NailDesignResponse.cs
@@ -0,0 +1,25 @@
+﻿using INBS.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.NailDesign
+{
+    public class NailDesignResponse
+    {
+        [Key]
+        public Guid DesignId { get; set; }
+
+        public string ImageUrl { get; set; } = Constants.DEFAULT_IMAGE_URL;
+
+        [Key]
+        public int NailPosition { get; set; } //4, 8, 12, 16, 20
+
+        [Key]
+        public bool IsLeft { get; set; } //true - left, false - right
+    }
+}
