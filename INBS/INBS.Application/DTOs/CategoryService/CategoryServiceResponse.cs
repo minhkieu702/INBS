0a8d90562b0a910de5b840423ed7a87855ea308a
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:26:08 +0700

    fix

---
+++ INBS/INBS.Application/DTOs/CategoryService/CategoryServiceResponse.cs
@@ -0,0 +1,22 @@
+﻿using INBS.Application.Services;
+using INBS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.CategoryService
+{
+    public class CategoryServiceResponse
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        [Key]
+        public Guid ServiceId { get; set; } = Guid.Empty;
+        //public virtual ServiceResponse? Service { get; set; }
+    }
+}
---
+++ INBS/INBS.Application/DTOs/Customer/CustomerRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.Customer
+{
+    public class CustomerRequest
+    {
+        public string? Description { get; set; }
+
+
+    }
+}
