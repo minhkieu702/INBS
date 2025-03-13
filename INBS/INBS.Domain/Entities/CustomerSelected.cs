74cc7ac29afad898fa826e034f82330560bdbb87
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:29:52 +0700

    change business

---
+++ INBS/INBS.Domain/Entities/CustomerSelected.cs
@@ -0,0 +1,36 @@
+﻿using INBS.Domain.Entities.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INBS.Domain.Entities
+{
+    public class CustomerSelected
+    {
+        public CustomerSelected()
+        {
+            ID = Guid.NewGuid();
+            IsFavorite = false;
+            Bookings = [];
+            NailDesignServiceSelecteds = [];
+            IsDeleted = false;
+        }
+
+        [Key]
+        public Guid ID { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public Guid CustomerID { get; set; }
+        [ForeignKey(nameof(CustomerID))]
+        [InverseProperty(nameof(Customer.CustomerSelecteds))]
+        public virtual Customer? Customer { get; set; }
+
+        [InverseProperty(nameof(NailDesignServiceSelected.CustomerSelected))]
+        public virtual ICollection<NailDesignServiceSelected> NailDesignServiceSelecteds { get; set; }
+
+        [InverseProperty(nameof(Booking.CustomerSelected))]
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
