d1bfd13f3da83da06954d5eeb85c6fe9ad90d5a1
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:30:09 +0700

    change business

---
+++ INBS/INBS.Domain/Entities/NailDesignServiceSelected.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INBS.Domain.Entities
+{
+    public class NailDesignServiceSelected
+    {
+        public Guid CustomerSelectedId { get; set; }
+        [ForeignKey(nameof(CustomerSelectedId))]
+        [InverseProperty(nameof(CustomerSelected.NailDesignServiceSelecteds))]
+        public virtual CustomerSelected? CustomerSelected { get; set; }
+
+        public Guid NailDesignServiceId { get; set; }
+        [ForeignKey(nameof(NailDesignServiceId))]
+        [InverseProperty(nameof(NailDesignService.NailDesignServiceSelecteds))]
+        public virtual NailDesignService? NailDesignService { get; set; }
+
+        public long Duration { get; set; }
+    }
+}
