929732c77f62b40f35140e4c8e4c3a39d80c9e2a
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-14 01:27:36 +0700

    change business

---
+++ INBS/INBS.Application/DTOs/Preference/PreferenceResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.DTOs.Preference
+{
+    public class PreferenceResponse
+    {
+        [Key]
+        public int ID { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid DesignId { get; set; }
+        public int PreferenceId { get; set; }
+        public string PreferenceType { get; set; } = string.Empty;
+
+        public object? Data { get; set; }
+    }
+}
