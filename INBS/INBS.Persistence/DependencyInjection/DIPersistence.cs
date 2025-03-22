ab5c8f5cf991500a02fc7d205705bfea0d967da2
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-22 11:09:35 +0700

    Update DIPersistence.cs

--- INBS/INBS.Persistence/DependencyInjection/DIPersistence.cs
+++ INBS/INBS.Persistence/DependencyInjection/DIPersistence.cs
@@ -21,6 +21,7 @@ namespace INBS.Persistence.DependencyInjection
                 options.UseLazyLoadingProxies()
                         //.UseSqlServer(Environment.GetEnvironmentVariable("connectionString"));
                         .UseSqlServer("Server = tcp:inbs.database.windows.net, 1433; Initial Catalog = inbsdatabase; Persist Security Info = False; User ID = inbsadmin; Password = String123!@#;MultipleActiveResultSets=False;Encrypt=false;TrustServerCertificate=true;Connection Timeout=30;");
+                //.UseSqlServer("Server=DESKTOP-54Q7719\\SQLEXPRESS; uid=sa; pwd=1234567890; database=INBSDatabase; TrustServerCertificate=True");
             });
             return services;
         }
