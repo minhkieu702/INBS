97c4f36430a402092570b4c223bb7484b9f7a29d
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-23 01:48:57 +0700

    add nail design service odata

---
+++ INBS/INBS.Application/IServices/INailDesignServiceService.cs
@@ -0,0 +1,15 @@
+﻿using INBS.Application.DTOs.NailDesignService;
+using INBS.Application.DTOs.NailDesignServiceSelected;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.IServices
+{
+    public interface INailDesignServiceService
+    {
+        IQueryable<NailDesignServiceResponse> Get();
+    }
+}
