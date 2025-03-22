7f6c9a20039eeb3d8977cde60b791be5944bcc79
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-03-23 01:48:06 +0700

    add nail design service selected odata

---
+++ INBS/INBS.API/Controllers/NailDesignServiceSelectedController.cs
@@ -0,0 +1,39 @@
+﻿using INBS.Application.DTOs.NailDesignService;
+using INBS.Application.DTOs.NailDesignServiceSelected;
+using INBS.Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace INBS.API.Controllers
+{
+    /// <summary>
+    /// Controller for handling Nail Design Service Selected operations.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NailDesignServiceSelectedController : ODataController
+    {
+        private readonly INailDesignServiceSelectedService _service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NailDesignServiceSelectedController"/> class.
+        /// </summary>
+        /// <param name="service">The service to handle Nail Design Service Selected operations.</param>
+        public NailDesignServiceSelectedController(INailDesignServiceSelectedService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Gets the list of Nail Design Service Selected.
+        /// </summary>
+        /// <returns>An <see cref="IQueryable{NailDesignServiceSelectedResponse}"/> of Nail Design Service Selected.</returns>
+        [HttpGet]
+        [EnableQuery(MaxExpansionDepth = 100)]
+        public IQueryable<NailDesignServiceSelectedResponse> Get()
+        {
+            return _service.Get();
+        }
+    }
+}
---
+++ INBS/INBS.Application/Services/NailDesignServiceSelectedService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using INBS.Application.DTOs.NailDesignServiceSelected;
+using INBS.Application.IServices;
+using INBS.Domain.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INBS.Application.Services
+{
+    public class NailDesignServiceSelectedService(IUnitOfWork _unitOfWork, IMapper _mapper) : INailDesignServiceSelectedService
+    {
+        public IQueryable<NailDesignServiceSelectedResponse> Get()
+        {
+			try
+			{
+				return _unitOfWork.NailDesignServiceSelectedRepository.Query().ProjectTo<NailDesignServiceSelectedResponse>(_mapper.ConfigurationProvider);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
