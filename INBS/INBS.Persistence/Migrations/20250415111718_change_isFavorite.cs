6b28e6d6e243df1b27e2664e371ef2e7b9db0083
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-04-15 22:08:29 +0700

    change some prop of database

---
+++ INBS/INBS.Persistence/Migrations/20250415111718_change_isFavorite.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INBS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_isFavorite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NailDesignServiceGuide");
+
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "CustomerSelected");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "CustomerSelected",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "NailDesignServiceGuide",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NailDesignServiceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NailDesignServiceGuide", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_NailDesignServiceGuide_NailDesignService_NailDesignServiceId",
+                        column: x => x.NailDesignServiceId,
+                        principalTable: "NailDesignService",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NailDesignServiceGuide_NailDesignServiceId",
+                table: "NailDesignServiceGuide",
+                column: "NailDesignServiceId");
+        }
+    }
+}
--- INBS/INBS.Persistence/Migrations/20250415150104_fix_somecode.Designer.cs
+++ INBS/INBS.Persistence/Migrations/20250415150104_fix_somecode.Designer.cs
@@ -4,6 +4,7 @@ using INBS.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace INBS.Persistence.Migrations
 {
     [DbContext(typeof(INBSDbContext))]
-    partial class INBSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415150104_fix_somecode")]
+    partial class fix_somecode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
