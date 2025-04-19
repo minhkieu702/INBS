350b2e44af0b435a718f0ffa4553a75a39a62606
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2025-04-19 19:08:44 +0700

    update reset password for customer

--- INBS/INBS.Application/IServices/IAuthenticationService.cs
+++ INBS/INBS.Application/IServices/IAuthenticationService.cs
@@ -10,7 +10,7 @@ namespace INBS.Application.IServices
         Task<LoginResponse> LoginStaff(string username, string password);
         Task RegisterCustomer(UserRequest request, string password, string confirmPassword);
         Task<string> ResetPasswordStaff(string username, string newPassword, string confirmPassword);
-        Task<string> ResetPasswordCustomer(string phone, string newPassword, string confirmPassword);
+        Task<LoginResponse> ResetPasswordCustomer(string phone,string otp, string newPassword, string confirmPassword);
         Task<LoginResponse> VerifyOtpAsync(string phoneNumber, string otp);
         Task ResendOtpAsync(string phoneNumber);
         Task ChangeProfile(UserRequest requestModel);
